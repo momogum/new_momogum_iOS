b8405856747c6506a46f9d6cc9d42508089041a1
SJM-coding <greatspirit637@gmail.com>
2025-03-27 16:38:06 +0900

    [FEAT] 카카오 로그인 세팅

---
+++ iOS_Momogum/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  iOS_Momogum
+//
+//  Created by 서재민 on 3/27/25.
+//
+
+import UIKit
+import Foundation
+import KakaoSDKCommon
+import KakaoSDKAuth
+
+class AppDelegate : UIResponder, UIApplicationDelegate {
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+            
+        print("kakaoAppKey : \(kakaoAppKey)")
+        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
+        
+        
+        
+        return true
+        
+    }
+    
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+          if (AuthApi.isKakaoTalkLoginUrl(url)) {
+              return AuthController.handleOpenUrl(url: url)
+          }
+
+          return false
+      }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        
+        return sceneConfiguration
+    }
+}
--- iOS_Momogum/iOS_MomogumApp.swift
+++ iOS_Momogum/iOS_MomogumApp.swift
@@ -6,9 +6,19 @@
 //
 
 import SwiftUI
-
+import KakaoSDKCommon
+import KakaoSDKAuth
 @main
 struct iOS_MomogumApp: App {
+   
+    
+    @UIApplicationDelegateAdaptor var appDelegate: AppDelegate
+//    init() {
+//        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+//            // Kakao SDK 초기화
+//        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
+//        }
+//    
     var body: some Scene {
         WindowGroup {
             ContentView()
